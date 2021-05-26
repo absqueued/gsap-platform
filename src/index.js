7d84423110346839605c10ab128112bd59527d9a
shekhardesigner <hello@shekhardesigner.com>
2021-05-26 15:32:20 +0545

    Airtime block styles completed

--- src/index.js
+++ src/index.js
@@ -5,5 +5,19 @@
 import './styles.scss';
 
 const log = (...m) => console.log(...m);
+import { gsap } from 'gsap';
+import { TextPlugin } from 'gsap/TextPlugin';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(TextPlugin, ScrollTrigger);
 
 log('Index Module', 'b');
+
+function initPlatform() {
+  log('initPlatform ... ');
+  gsap.registerPlugin(ScrollTrigger);
+}
+
+if (gsap) {
+  initPlatform();
+}
